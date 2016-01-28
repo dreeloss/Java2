ecde3fa61370d7e2e4da000d1d1ba6ec737bcae6
Amadou Barinux <barryamadou91@gmail.com>
2016-01-28 07:30:58 -0800

    Fifth

--- Helloworld.java
+++ Helloworld.java
@@ -1,10 +1,5 @@
 public class Helloworld { 
    public static void main(String[] args) { 
-<<<<<<< HEAD
-      System.out.println("za,nazjkhejkazejnjknHello3, Worldqsdkqsldlqskdmlqsmldkqslkls");
-=======
-      System.out.println("Hello1 new modele,  amadou World");
->>>>>>> newmaster/master
       System.out.println("Hello0, World");
 	System.out.println("final, World");
    }
