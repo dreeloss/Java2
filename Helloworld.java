df7ed2fda2cae40f13fe733df755ee51aa1b3e05
El Chalouhi <farid.el.chalouhi@accenture.com>
2016-01-28 16:45:28 +0100

    fgo

--- Helloworld.java
+++ Helloworld.java
@@ -3,18 +3,10 @@ public class Helloworld {
       System.out.println("za,nazjkhejkazejnjknHello3, Worldqsdkqsldlqskdmlqsmldkqslkls");
       System.out.println("Hello1 new modele,  amadou World");
       System.out.println("Hello0, World");
-<<<<<<< HEAD
+
 	System.out.println("Hello0Farid said hi, World");
 	System.out.println("git plz build");
 	dqsjdjkqsndjk
-=======
 	System.out.println("final, World");
->>>>>>> newmaster/master
    }
 }
-<<<<<<< HEAD
-
-
-=======
-go
->>>>>>> 0cd0c8d5ac90b53fd3e93ebb8f7b65defa1d47fc
