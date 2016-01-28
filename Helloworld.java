022379616bde5bdfdf0252f887ea47e261a56d8e
El Chalouhi <farid.el.chalouhi@accenture.com>
2016-01-28 16:02:54 +0100

    farid new commits

--- Helloworld.java
+++ Helloworld.java
@@ -1,7 +1,8 @@
 public class Helloworld { 
    public static void main(String[] args) { 
-      System.out.println("Hello3, World");
+      System.out.println("za,nazjkhejkazejnjknHello3, Worldqsdkqsldlqskdmlqsmldkqslkls");
       System.out.println("Hello0, World");
 	System.out.println("Hello0Farid said hi, World");
    }
 }
+go
