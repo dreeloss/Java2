150d007dd50310faeeda79818063b52d08be2eee
El Chalouhi <farid.el.chalouhi@accenture.com>
2016-01-28 15:45:16 +0100

    farid modif

--- Helloworld.java
+++ Helloworld.java
@@ -2,5 +2,6 @@ public class Helloworld {
    public static void main(String[] args) { 
       System.out.println("Hello3, World");
       System.out.println("Hello0, World");
+	System.out.println("Hello0Farid said hi, World");
    }
 }
