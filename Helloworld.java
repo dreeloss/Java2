e9428bf124f1d0c2cfc0a893ff9d1414bf1e6ee8
El Chalouhi <farid.el.chalouhi@accenture.com>
2016-01-28 16:29:03 +0100

    testing

--- Helloworld.java
+++ Helloworld.java
@@ -1,12 +1,11 @@
 public class Helloworld { 
    public static void main(String[] args) { 
-<<<<<<< HEAD
       System.out.println("za,nazjkhejkazejnjknHello3, Worldqsdkqsldlqskdmlqsmldkqslkls");
-=======
       System.out.println("Hello1 new modele,  amadou World");
->>>>>>> newmaster/master
       System.out.println("Hello0, World");
 	System.out.println("Hello0Farid said hi, World");
+	System.out.println("git plz build");
+	dqsjdjkqsndjk
    }
 }
 go
