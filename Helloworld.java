541d0fcc54c4d3fc5a7f2445592f4f2742c76ca3
Barry <amadou.barry@accenture.com>
2016-02-01 11:27:20 +0100

    Signed-off-by: Barry <amadou.barry@accenture.com>

--- Helloworld.java
+++ Helloworld.java
@@ -2,7 +2,7 @@ public class Helloworld {
    public static void main(String[] args) { 
 
       System.out.println("Hello4, World");
-      System.out.println("Hello0, World");
+      System.out.println("Hello5, World");
       
    }
 }
